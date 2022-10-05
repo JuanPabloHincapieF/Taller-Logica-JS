7e3afed68f2ed0b419a4bd3785b5669a6d791f33
JuanPabloHincapieF <jphf0527@gmail.com>
2022-10-05 18:49:09 -0500

    Ejercicio 3

--- ejercicio 3/main.js
+++ ejercicio 3/main.js
@@ -1,3 +1,22 @@
 // Ejericicio 3
 
 // Tiendas ACME desea un programa que se ingrese en monto de varias ventas, al ingresar cada una de las ventas debe mostrarse un descuento del 7% y el monto que se paga en dicha venta (al decir varias ventas nos referimos a n ventas).
+
+// cuantas ventas deseas ingresar
+// aplicarle descuento
+// mostrar la venta con el descuento
+
+let cont = 0;
+function tomarVenta(e) {
+  if (e.key === "Enter") {
+    cont++;
+    let venta = document.querySelector(".inputVenta").value;
+    let descuento = venta * 0.93;
+    document.querySelector(".tableVenta").innerHTML += `
+    <tr>
+    <th scope="row">${cont}</th>
+    <td>${venta}</td>
+    <td>${descuento}</td>
+  </tr>`;
+  }
+}
